7f215fbb76cdbf36f85aba138f4a78be28b9d607
Albert Nafría <albertnafria@icloud.com>
2020-05-22 22:50:03 +0200

    few changes

---
+++ .bash_profile
@@ -0,0 +1,9 @@
+# .bash_profile
+
+if [ -f ~/.bashrc ]; then
+		. ~/.bashrc
+fi
+
+# Incluir ~/bin en variable PATH
+PATH=$PATH:~/bin
+export PATH
---
+++ symLinks.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+# Symbolic links of the dotfiles in this directory
+
+ln -s -f ~/dotfiles/.vimrc ~/.vimrc
+ln -s -f ~/dotfiles/.bashrc ~/.bashrc
+ln -s -f ~/dotfiles/.bash_profile ~/.bash_profile
