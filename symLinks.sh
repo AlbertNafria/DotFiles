2b561e90bde9505505fff9a010d47a062558c76f
Albert Nafría <albertnafria@icloud.com>
2020-09-07 13:46:43 +0200

    modificacion en el script

--- symLinks.sh
+++ symLinks.sh
@@ -6,3 +6,5 @@ ln -s -f ~/dotfiles/.vimrc ~/.vimrc
 ln -s -f ~/dotfiles/.bashrc ~/.bashrc
 ln -s -f ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s -f ~/dotfiles/.vim/ ~/.vim
+ln -s -f ~/dotfiles/.newsboat/ ~/.newsboat
+ln -s -f ~/dotfiles/.tmux.conf/ ~/.tmux.conf
